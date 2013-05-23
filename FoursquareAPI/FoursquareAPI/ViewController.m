84e5c525ad8cbe7bf83dad619245ed29ca312240
andrewmc2 <acl.mccallum2@gmail.com>
2013-05-23 15:12:33 -0500

    andrew added another log

--- FoursquareAPI/FoursquareAPI/ViewController.m
+++ FoursquareAPI/FoursquareAPI/ViewController.m
@@ -20,6 +20,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"andrew was here");
+    NSLog(@"andrew was here again");
 }
 
 - (void)didReceiveMemoryWarning
